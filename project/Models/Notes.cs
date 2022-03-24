91ee29ac28418e6b92f5623d928606ef4ae2e746
Joe Fong <joe.fong@madetech.com>
2022-03-24 16:35:57 +0000

    refine the model for Notes

--- project/Models/Notes.cs
+++ project/Models/Notes.cs
@@ -1,13 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace project.Models;
 
 public class Note
 {
     public int id { get; set; }
 
+    [Required]
     public string Title { get; set; } = "";
 
+    [Required]
     public string Content { get; set; } = "";
 
-    public DateTime created_at { get; set; }
+    public DateTime created_at { get; set; } = DateTime.UtcNow;
     
 }
