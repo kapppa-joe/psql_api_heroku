398402f6ef7443457c55780086ac1916f36711c4
Joe Fong <joe.fong@madetech.com>
2022-03-28 09:41:39 +0100

    put main server code in a method

--- project/Program.cs
+++ project/Program.cs
@@ -2,44 +2,53 @@ using project.Data;
 using project.Models;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
-if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+namespace project;
+
+public class Program
 {
-    // for dev server, get the db conn string from env or setting file.
-    connectionString ??= builder.Configuration.GetConnectionString("DefaultConnection");
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+        if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+        {
+            // for dev server, get the db conn string from env or setting file.
+            connectionString ??= builder.Configuration.GetConnectionString("DefaultConnection");
+        }
+        else
+        {
+            // if it is production, get the connection string from ENV VAR that Heroku supplies.
+            connectionString = ConfigurationHelper.GetHerokuConnectionString();
+        }
+
+        builder.Services.AddDbContext<PsqlDbContext>(options =>
+            options.UseNpgsql(connectionString)
+        );
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-else
-{
-    // if it is production, get the connection string from ENV VAR that Heroku supplies.
-    connectionString = ConfigurationHelper.GetHerokuConnectionString();
-}
-
-builder.Services.AddDbContext<PsqlDbContext>(options => 
-    options.UseNpgsql(connectionString)
-);
-
-// Add services to the container.
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
