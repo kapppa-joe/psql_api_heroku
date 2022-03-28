a03ec220c5c5f33ba68c913e961f0fc74f2f3de1
Joe Fong <joe.fong@madetech.com>
2022-03-28 13:48:27 +0100

    comment out endpoints for delete & put (to wait for tests of those endpoints)

--- project/Controllers/NotesController.cs
+++ project/Controllers/NotesController.cs
@@ -47,4 +47,54 @@ public class NotesController : ControllerBase
 
         return CreatedAtAction("GetNote", new {id = note.id}, note);
     }
+
+    // [HttpDelete("{id}")]
+    // public async Task<IActionResult> DeleteNote(int id)
+    // {
+    //     var note = await _context.Notes.FindAsync(id);
+    //     if (note == null)
+    //     {
+    //         return NotFound();
+    //     }
+    //
+    //     _context.Notes.Remove(note);
+    //     await _context.SaveChangesAsync();
+    //
+    //     return NoContent();
+    // }
+    //
+    // [httpPut("{id}")]
+    // public async Task<IActionResult PutNote(int id, Note note)
+    // {
+    //     if (id != note.id)
+    //     {
+    //         return BadRequest();
+    //     }
+    //
+    //     _context.Entry(note).State = EntityState.Modified;
+    //
+    //     try
+    //     {
+    //         await _context.SaveChangesAsync();
+    //     }
+    //     catch (DbUpdateConcurrencyException)
+    //     {
+    //         if (!NoteExists(id))
+    //         {
+    //             return NotFound();
+    //         }
+    //         else
+    //         {
+    //             throw;
+    //         }
+    //     }
+    //
+    //     return NoContent();
+    // }
+    //
+    // private bool NoteExists(int id)
+    // {
+    //     return _context.Notes.Any(e => e.id == id);
+    // }
+    
 }
