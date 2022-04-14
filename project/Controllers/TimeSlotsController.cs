1f850f995fb3ccf3924a4b6983bfe3a8240932d8
Joe Fong <joe.fong@madetech.com>
2022-04-14 16:21:26 +0100

    change timeslot from next 7 days to next 28 days

--- project/Controllers/TimeSlotsController.cs
+++ project/Controllers/TimeSlotsController.cs
@@ -12,12 +12,12 @@ public class TimeSlotsController : ControllerBase
     public ActionResult GetTimeslots()
     {
         var today = DateTime.Today;
-        var nextSevenDays = Enumerable.Range(1, 7).Select(x =>
+        var next28Days = Enumerable.Range(1, 28).Select(x =>
         {
             var date = new DateTime(today.Year, today.Month, today.Day + x, 11, 0, 0);
             return date;
         });
-        var workDaysOnly = nextSevenDays.Where(date =>
+        var workDaysOnly = next28Days.Where(date =>
             date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday);
 
         string[] timeslots = workDaysOnly.Select(date => date.ToString("yyyy-MM-ddThh:mm")).ToArray();
